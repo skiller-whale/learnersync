5061de543f488fd91e1260abd032142639032a30
Matthew Bloch <matthew@bloch.tv>
2022-08-20 23:46:49 +0100

    First exercises in Collections & Generics module, temporarily use the Go sync script until the Java script is tested.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/skillerwhale/sync-go
+
+go 1.16
+
+require github.com/skiller-whale/sync-go v0.1.0
---
+++ sync.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/skiller-whale/sync-go/sync"
+)
+
+func main() {
+	sync.Start()
+}
