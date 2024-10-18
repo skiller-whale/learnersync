0f0e8b4667db3a09780b30bec00fe9349c95c218
Dave Millican <dave@skillerwhale.com>
2024-10-18 15:38:40 +0100

    Add a mock server to the test function

--- tdd/ping_test.go
+++ tdd/ping_test.go
@@ -1,7 +1,15 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestSendPingMakesPostRequest(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    })
+	httptest.NewServer(handler)
+
 	SendPing()
 }
