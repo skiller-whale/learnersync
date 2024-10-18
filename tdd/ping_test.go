dc7fda415699ce6b77dcf5d5921f0f932478badb
Dave Millican <dave@skillerwhale.com>
2024-10-18 16:38:00 +0100

    Make tests fail with a timeout

--- tdd/ping_test.go
+++ tdd/ping_test.go
@@ -7,10 +7,14 @@ import (
 )
 
 func TestSendPingMakesPostRequest(t *testing.T) {
+	requests := make(chan *http.Request, 1)
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requests <- r
     })
 	server := httptest.NewServer(handler)
 
 	err := SendPing(server.URL)
 	if err != nil { t.Error("SendPing raised an error") }
+
+	<- requests  // Get the first request
 }
